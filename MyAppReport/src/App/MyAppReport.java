ffdc3d344cd1b3de43d3e532eae4b7edfe0e48e2
14-AB105LA <14-AB105LA@10.12.19.187>
2017-04-21 07:23:36 -0500

    3er Commit

--- MyAppReport/src/App/MyAppReport.java
+++ MyAppReport/src/App/MyAppReport.java
@@ -8,21 +8,56 @@ import java.util.Scanner;
  */
 public class MyAppReport {
 
-  
     public static void main(String[] args) {
-       
+
         Scanner sc = new Scanner(System.in);
-        Operaciones operaciones = new Operaciones();
-        
-        System.out.println("Ingresa el 1er número para la operación suma");
-        int num1 = sc.nextInt();
-        System.out.println("Ingresa el segundo número para la operación suma");
-        int num2 = sc.nextInt();
-        
-        System.out.println("El resultado es: " + operaciones.suma(num1, num2));
-        
-        
-        
+        Operaciones operaciones = null;
+        System.out.println("Ingrese la operación (+,-,*,/)");
+        String operacion = sc.nextLine();
+
+        switch (operacion) {
+
+            case "+":
+                System.out.println("Ingresa el 1er número para la operación suma");
+                int sum1 = sc.nextInt();
+                System.out.println("Ingresa el segundo número para la operación suma");
+                int sum2 = sc.nextInt();
+                operaciones = new Operaciones();
+                System.out.println("El resultado es: " + operaciones.suma(sum1, sum2));
+                break;
+
+            case "-":
+                System.out.println("Ingresa el 1er número para la operación resta");
+                int res1 = sc.nextInt();
+                System.out.println("Ingresa el segundo número para la operación resta");
+                int res2 = sc.nextInt();
+                operaciones = new Operaciones();
+                System.out.println("El resultado es: " + operaciones.resta(res1, res2));
+                break;
+
+            case "*":
+                System.out.println("Ingresa el 1er número para la operación multiplicación");
+                int mul1 = sc.nextInt();
+                System.out.println("Ingresa el segundo número para la operación multiplicación");
+                int mul2 = sc.nextInt();
+                operaciones = new Operaciones();
+                System.out.println("El resultado es: " + operaciones.multiplicacion(mul1, mul2));
+                break;
+
+            case "/":
+                System.out.println("Ingresa el 1er número para la operación divición");
+                int div1 = sc.nextInt();
+                System.out.println("Ingresa el segundo número para la operación divición");
+                int div2 = sc.nextInt();
+                operaciones = new Operaciones();
+                System.out.println("El resultado es: " + operaciones.divicion(div1, div2));
+                break;
+
+            default:
+                System.out.println("No es una operación valida");
+                break;
+        }
+
     }
-    
+
 }
--- MyAppReport/src/App/Operaciones.java
+++ MyAppReport/src/App/Operaciones.java
@@ -12,4 +12,25 @@ public class Operaciones {
         
         return (num1 + num2);
     }
+    
+    
+     public int resta(int num1,int num2){
+        
+        
+        return (num1 - num2);
+    }
+     
+     
+      public int multiplicacion(int num1,int num2){
+        
+        
+        return (num1 * num2);
+    }
+      
+      
+       public int divicion(int num1,int num2){
+        
+        
+        return (num1 / num2);
+    }
 }
